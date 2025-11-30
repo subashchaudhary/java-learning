194450adfc9ffea0f9b9fa832b47dd558b9e3a01
subashchaudhary <subashtharu116@gmail.com>
2025-11-30 11:30:02 +0545

    new file added

---
+++ src/collectionframework/CollectionFrameworkDemo.java
@@ -0,0 +1,10 @@
+package collectionframework;
+
+public class CollectionFrameworkDemo {
+
+    static void main() {
+
+
+        System.out.println("Learning on The Collection Framework");
+    }
+}
